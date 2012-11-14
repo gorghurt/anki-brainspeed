39d6d5c4b91c366462fad12bb713a4878f8e210d
patrick <patrick@patrick-MS-7360.(none)>
2012-11-14 20:44:09 +0100

    zufällige belegung der buttons und fragen antworten aus listen

--- experimente/quiz.py
+++ experimente/quiz.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""
-ZetCode PyQt4 tutorial 
 
-In this example, we position two push
-buttons in the bottom-right corner 
-of the window. 
 
-author: Jan Bodnar
-website: zetcode.com 
-last edited: October 2011
-"""
 
 import sys
 from PyQt4 import QtGui
 import random
 
-Anzahl_Antworten= 4
+Anzahl_Antworten= 2
+questions=["a?","b?","c?","d?","e?","f","g","h","i","j"]
+answers=["a?","b?","c?","d?","e?","f","g","h","i","j"]
 
-class Window(QtGui.QWidget):
-    btnlist = []
-    
+class Window(QtGui.QWidget):    
     def __init__(self):
         super(Window, self).__init__()
         
@@ -29,57 +20,44 @@ class Window(QtGui.QWidget):
         
     def initUI(self):
 	n=Anzahl_Antworten
-        grid=QtGui.QGridLayout()
-        label=QtGui.QLabel("blargh")
-        timelabel=QtGui.QLabel("time")
-        lebenlabel=QtGui.QLabel("Leben")
-        label.setSizePolicy(QtGui.QSizePolicy( QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding))
-        okButton = QtGui.QPushButton()
-        cancelButton = QtGui.QPushButton("Cancel")
-	okButton.setText("bla")
-	okButton.setSizePolicy(QtGui.QSizePolicy( QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding))
-	cancelButton.setSizePolicy(QtGui.QSizePolicy( QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding))
-	okButton.clicked.connect(lambda: self.right())
-	cancelButton.clicked.connect(lambda: self.wrong())
-        hbox = QtGui.QHBoxLayout()
-        #hbox.addStretch(1)
-        #grid.addWidget(okButton,2,0)
-        #grid.addWidget(cancelButton,2,1)
 	
-        vbox = QtGui.QVBoxLayout()
-        #vbox.setStretch(2,1)
-        grid.addWidget(timelabel,0,n-1)
-        grid.addWidget(lebenlabel,0,0)
-        grid.addWidget(label,1,0,1,n-1)
-        #vbox.setStretch(2,1)
+	#grid Layout:
+        grid=QtGui.QGridLayout()
+        
+        #Label 
+        self.label=QtGui.QLabel("Frage")
+        self.timelabel=QtGui.QLabel("time")
+        self.lebenlabel=QtGui.QLabel("Leben")
+        
+        self.label.setSizePolicy(QtGui.QSizePolicy( QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding))
+
+	#Label ins Grid einfügen
+        grid.addWidget(self.timelabel,0,n-1)
+        grid.addWidget(self.lebenlabel,0,0)
+        grid.addWidget(self.label,1,0,1,n-1)
+
+        #Stretch werte Setzen (Fragen label und Butttons sollen größer sein als Rest)
         grid.setRowStretch(1,3)
         grid.setRowStretch(2,2)
-        vbox.addLayout(hbox)
-        #hbox.setSizePolicy(QtGui.QSizePolicy( QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding))
+
+
         #Dynamisches Button erzeugen
         self.btnlist=[]
         
         for i in range(0,n) :
-	  #Button=QtGui.QPushButton("bla")
-	  #Button.setSizePolicy(QtGui.QSizePolicy( QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding))
-	  #grid.addWidget(Button,2,i)
-	 # command=lambda x=i: self.right(x)
-	  #Button.clicked.connect(command)
-	  #Button.clicked.connect(lambda: self.check())
-	  #print i
-	  #x= int(i)
+
 	  self.btnlist.append(QtGui.QPushButton(str(i)))
 	  
-	  #self.btnlist[i].clicked.connect(lambda: self.right(i))#  klappt nicht, alle geben das gleiche aus daher anders
-	 # self.btnlist[i].clicked.connect(lambda value=i : self.check(value))
 	  self.btnlist[i].setSizePolicy(QtGui.QSizePolicy( QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding))
 	  grid.addWidget(self.btnlist[i],2,i)
-	  
+	
+	#Fenster erstellen
         self.setLayout(grid)    
+        #erster Refresh  (später entfernen und durch start dialog ersetzen, oder dieses fesnter nach start dialog erstellen)
         self.refresh(True)
         
         self.resize(640,480)
-        self.setWindowTitle('Buttons')    
+        self.setWindowTitle('Anki-BrainSpeed')    
         self.show()
         
     def check(self,i):
@@ -95,18 +73,28 @@ class Window(QtGui.QWidget):
       print("wrong")
       
       
-    def refresh(self,first=False):
-      r=random.randint(0,Anzahl_Antworten)
+    def refresh(self, first=False):
+      right = random.randint(0, len(answers)-1 )
+      self.label.setText(questions[right])
+      r=random.randint(0, Anzahl_Antworten-1)
       print("refresh"+ str(r))
-      for i in range(0,Anzahl_Antworten):
+      for i in range(0, Anzahl_Antworten):
 	if i == r:
 	  if first != True:
 	    self.btnlist[i].clicked.disconnect()
 	  self.btnlist[i].clicked.connect(lambda: self.right())
+	  self.btnlist[i].setText(answers[right])
 	else:
 	  if first != True:
 	    self.btnlist[i].clicked.disconnect()
 	  self.btnlist[i].clicked.connect(lambda: self.wrong())
+	  wrong=random.randint(0,len(answers)-1)
+	  if wrong==right:
+	      if wrong+1 < len(answers):
+		wrong=wrong+1
+	      else:
+		wrong=wrong-1
+	  self.btnlist[i].setText(answers[wrong])
 
 def main():
     
